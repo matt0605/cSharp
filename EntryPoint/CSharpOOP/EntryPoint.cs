d921582e842e701270ba1c63f8ec973f80475690
matt0605 <szjava@ymail.com>
2019-01-05 22:11:24 -0500

    Adding C# files

---
+++ EntryPoint/CSharpOOP/EntryPoint.cs
@@ -0,0 +1,30 @@
+﻿using Characters.Warriors;  //folder
+using System;
+
+
+
+
+
+ public class EntryPoint
+    {
+        static void Main()
+        {
+        Warrior theGoodGuy = new Warrior(190, 80, "Sean");
+        //theGoodGuy.Height = 190;
+        //theGoodGuy.Weight = 80;
+        //theGoodGuy.Name = "Sean";
+
+        Warrior theBadGuy = new Warrior(195, 85, "Satan");
+        //theBadGuy.Height = 195;
+        //theBadGuy.Weight = 85;
+        //theBadGuy.Name = "Satan";
+
+        Console.WriteLine(theGoodGuy.Height);
+        Console.WriteLine(theBadGuy.Height);
+
+        theGoodGuy.Greetings(theBadGuy);
+        theBadGuy.Greetings(theGoodGuy);
+
+    }
+    }
+
