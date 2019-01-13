c204ab2bd9b77cab8f4646b2508bc785d8711f11
matt0605 <szjava@ymail.com>
2019-01-13 11:39:25 -0500

    adding C#Beginners repository

---
+++ C#Beginners/CSharpConsole/Program.cs
@@ -0,0 +1,412 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+class EntryPoint
+{
+
+    static void Main()
+    {
+        ////Difference Between Write/Writeln
+        ////console write is for a single role or same role or line
+        //Console.Write("I am learning ");
+        //Console.Write("C# from ");
+        ////Console.Write("Udemy.");
+        //Console.Write("Udemy.\n"); //\n provides a new line
+        ////or
+        //Console.WriteLine(); //gives you next line 
+
+        ////console writeln provides a new line
+        //Console.WriteLine("I am learning ");
+        //Console.WriteLine("C# from ");
+        //Console.WriteLine("Udemy.");
+
+        //formating string
+        //string freshJuice = "Lets make fresh juice from Bananas";
+        //string strawberry = "strawberry";
+        //int five = 5;
+        //float fiveFloat = 500.5666f;
+
+
+        //Console.WriteLine("{0} and {1}", freshJuice, strawberry);
+        //Console.WriteLine("{0, 12}", strawberry); //the 12 represents the length of field to hold strawberry
+        //Console.WriteLine("{0, 8}", strawberry); //the 8 represents the length of field to hold strawberry
+        //Console.WriteLine("{0, 10}", five); //the five will start in the 10th space
+        //Console.WriteLine("{0, 10:C}", five); //C stands for currency
+        //Console.WriteLine("{0, 10:C1}", five); // add 1 number after the decimal point
+        //Console.WriteLine("{0, 10:C1}", five); // add 1 number after the decimal point
+        //Console.WriteLine("{0, 10:C0}", five); // add 0 number after the decimal point
+        //Console.WriteLine("{0, 10:C3}", fiveFloat); // add 0 number after the decimal point
+
+        //Escaping  Special Character
+
+        //Console.WriteLine("C:\Users\szeon\Documents\text.txt");  //incorrect
+        // escape by adding  a backslash, diplay the next character after the backslash
+        //Console.WriteLine("C:\\Users\\szeon\\Documents\\text.txt");  //correct
+
+        //add a @ to avoid adding a backslash
+        // Console.WriteLine(@"C:\Users\szeon\Documents\text.txt");  //correct
+
+        // reqarding quotation marks, add a backslash infront of the quotation mark
+        //Console.WriteLine("Some text that needs \"quotation marks\"!");
+
+        // if you use the @ symbol use double quotation mark
+        //escape the quotation marks with quotation mark
+        //Console.WriteLine(@"Some text that needs ""quotation marks""!");
+
+        //no escape is needed for single quotaion marks
+        // Console.WriteLine(@"Some text that needs 'single quotation marks'!");
+
+
+        //Read a character from the console
+
+        // Console.Read(); //let us write a character or more on the console
+
+        //Console.WriteLine("How old are you?");
+        // int age = Console.Read();
+        //Console.WriteLine("I am " + age + "years old");  // this will state 50 years old, from the ACII table if you type 25 
+
+        // you have to use cast typing
+        //Console.WriteLine("How old are you?");
+        //int age = Console.Read();
+        //char theAge = (char)age;
+        //or to save  a lot writing 
+        //char theAge = (char)Console.Read();  //will return the ascii  50 and continue with type casting  to char
+
+        // Console.WriteLine("I am " + theAge + " years old");  // this will state 50 years old, from the ACII table 
+
+
+        /**************************/
+        //Read a line from the console
+
+        // Console.WriteLine("How old are you?");
+        //string age = Console.ReadLine();  //can  use more than 1 charater
+
+        // Console.WriteLine("I am " + age + " years old");
+
+        // // if you want to work with the String you must parse or type cast
+
+        // int theNewAge = int.Parse(age) + 1; ;
+        // Console.WriteLine();
+        // Console.WriteLine(theNewAge);
+
+        /*****************/
+        // Read a pressed key from the Console        
+
+        // ConsoleKeyInfo myKey = Console.ReadKey();
+
+        //Console.WriteLine(myKey.Key);
+
+        //Console.WriteLine("\nYou clicked: " + myKey.Key);
+        //Console.WriteLine("\nYou clicked: " + myKey.KeyChar);   //will provide the correct case
+
+        //Console.WriteLine("\nYou clicked: " + myKey.Key);
+        //Console.WriteLine("\nThe Modifier is : " +  myKey.Modifiers);
+
+        /*****************/
+        //Change the color o the console
+        //Console.ForegroundColor = ConsoleColor.Magenta; //changes everything on the following row
+        //Console.BackgroundColor = ConsoleColor.DarkRed; //changes everything on the following row
+        //Console.Clear();    // clears everythih from the coonsole,change the whole background color
+
+        //Console.WriteLine("something red blue green yellow");
+        //Console.ForegroundColor = ConsoleColor.Red;
+        //Console.BackgroundColor = ConsoleColor.DarkRed; //changes everything on the following row
+        //Console.WriteLine("something red blue green yellow");
+        //Console.ForegroundColor = ConsoleColor.White;
+        //Console.BackgroundColor = ConsoleColor.DarkYellow; //changes everything on the following row
+        //Console.WriteLine("something red blue green yellow");
+        //Console.ForegroundColor = ConsoleColor.Blue;
+        //Console.BackgroundColor = ConsoleColor.DarkGreen; //changes everything on the following row
+        //Console.Write("something red blue green yellow"); //same row
+        //Console.ForegroundColor = ConsoleColor.Green;
+        //Console.BackgroundColor = ConsoleColor.DarkBlue; //changes everything on the following row
+        //Console.Write("something red blue green yellow"); //same row
+
+        ////get the default color
+        //Console.ForegroundColor = ConsoleColor.White;
+        ////or
+        //Console.ResetColor();
+        //Thread.Sleep(2000);  //use System.Threading
+        //Console.Clear(); //clears everything from the console
+
+        /*****************/
+        //Change the cursor in the console
+        //Console.CursorVisible = false; //makes it invisible
+        //Console.WriteLine("text text text text text, abc abc");
+        //Thread.Sleep(2000);
+        //Console.CursorVisible = true; //makes it visible
+        //Console.WriteLine("text text text text text, abc abc");
+
+
+        //where to start typing
+        //Console.SetCursorPosition(5, 5); // 5 rows down and 5 space from margin
+        //Console.WriteLine("text text text text text, abc abc");
+
+        //change the title on the console
+        //Console.Title = "Console Title Example";  //title of the Console
+
+        //how to position, change size of console
+
+        //Console.WindowHeight = 20;  //change the height
+        //Console.WindowWidth = 50;  //change the width
+        //or
+        //Console.SetWindowSize(50, 20); //width or height change at the same time
+        //Console.SetWindowSize(Console.LargestWindowWidth, Console.LargestWindowHeight, );
+        //change the rows and columns
+        //Console.BufferHeight = 200; //also removes scroll bar
+        //Console.BufferWidth = 500;
+        //or
+        //Console.SetBufferSize(500, 200); //width and height
+        //Console.WindowLeft = 10; // move the scroller x time to right 
+        //Console.WindowTop = 5;  //move the scroller x time from the top
+        // or
+        //Console.SetWindowPosition(10, 5); // left and top, //will get error if there are no scroller
+        //Console.WriteLine("text text text text text, abc abc");
+        //Console.WriteLine("text text text text text, abc abc");
+        //Console.WriteLine("text text text text text, abc abc");
+        //Console.WriteLine("text text text text text, abc abc");
+        //Console.WriteLine("text text text text text, abc abc");
+
+        //buffer size can't be smaller than the window size
+
+        //max and min size of screen
+        //Console.WriteLine(Console.LargestWindowHeight);
+        //Console.WriteLine(Console.LargestWindowWidth);
+        //Console.SetWindowSize(Console.LargestWindowWidth,Console.LargestWindowHeight);
+        // Console.SetBufferSize(Console.LargestWindowWidth, Console.LargestWindowHeight);
+
+        /**Quiz**/
+
+        //15 character strings and you want 7 extra space on the left
+        //string someString = "123456789012345";
+        //Console.WriteLine("{0, 22}", someString);
+
+        //escape forbiddin characters
+        //By using the \ right before the forbidden character or @ infront of the whole string 
+
+        //BufferSize controls how much we would be able to scroll vertically and horizontally; 
+        //WindowSize controls how many rows and columns will be visible from the console at a given time
+        //WindowPosition controls how many rows and columns we will be offsetted from the top left corner of the console
+        // when we start or program
+
+
+        /**** Exercise  ******/
+
+        /*1. Create sevearal strings that contain words and sentences. Utilize Write and WriteLine to correctly construct sentences
+        from the wors, print them and print the already constructed sentences. Do Not us string concatenation*/
+
+
+        //string stringA = "This";
+        //string stringB = " is";
+        //string stringC = " my";
+        //string stringD = " sentence.";
+        //string sentence = "This is my sentence.";
+
+        //Console.Write(stringA);
+        //Console.Write(stringB);
+        //Console.Write(stringC);
+        //Console.Write(stringD);
+        //Console.WriteLine();
+        //Console.WriteLine(sentence);
+
+
+
+        /*2.Take the string given in the Hints:2) section and correctly escape all characters that need escaping, so that you can output
+        it on the console as shown in the screenshot
+        input: Console.Writeline("C:\Program Files\"Microsoft Programs"\'VisualStudio'");
+         
+         */
+        //Console.WriteLine("C:\\Program Files\\\"Microsoft Programs\"\\\'VisualStudio\'");
+
+
+
+
+
+
+        /*3.  Ask two inputs from the user "Name of the Program: (string) and "Local Drive Letter:" (character),use the information that was input
+        to reconstruct your string from the previous exercise*/
+
+        //Console.WriteLine("Name of the Program:");
+        //string nameOfProgram = Console.ReadLine();
+        //Console.WriteLine("Local Drive Letter:");
+        //ConsoleKeyInfo myKey = Console.ReadKey();
+        //Console.WriteLine();
+        //Console.WriteLine(myKey.KeyChar + ":\\Program Files\\\"Microsoft Programs\"\\\'" + nameOfProgram + "\'");
+
+
+
+        /* 4. You are given a set of strings and numbers. Align them in such way that they look like a columns from a table, limit the numbers after
+         the decimal point to only 2 numbers and include a currency for the total*/
+
+
+        //string title = "Protein Intake Week: 1";
+        //string pipe = "|";
+        //string horLine = "======================";
+        //float firstFloat = 80.88557f;
+        //float secondFloat = 94.56465f;
+        //float thirdFloat = 78.67893f;
+        //float fourthFloat = 88.66654f;
+        //float fifthFloat = 88.6466f;
+        //float sixthFloat = 76.777f;
+        //float seventhFloat = 91.85759f;
+        //float totalFloat = 55.77853f;
+
+        //Console.WriteLine(pipe + title + pipe);
+        //Console.WriteLine(pipe + horLine + pipe);
+        //Console.Write(pipe + "{0, 22:C2}", firstFloat);
+        //Console.WriteLine(pipe);
+        //Console.Write(pipe + "{0, 22:C2}", secondFloat);
+        //Console.WriteLine(pipe);
+        //Console.Write(pipe + "{0, 22:C2}", thirdFloat);
+        //Console.WriteLine(pipe);
+        //Console.Write(pipe + "{0, 22:C2}", fourthFloat);
+        //Console.WriteLine(pipe);
+        //Console.Write(pipe + "{0, 22:C2}", fifthFloat);
+        //Console.WriteLine(pipe);
+        //Console.Write(pipe + "{0, 22:C2}", sixthFloat);
+        //Console.WriteLine(pipe);
+        //Console.Write(pipe + "{0, 22:C2}", seventhFloat);
+        //Console.WriteLine(pipe);        
+        //Console.WriteLine(pipe + horLine + pipe);
+        //Console.Write(pipe + "Total" + "{0, 17:C2}", totalFloat);
+        //Console.WriteLine(pipe);
+
+
+
+
+        /* 5. Take the example from the previous exercise and colorize it. Add color to the characters and add different backgroung for the borders,
+         separations, title, data and total. Optionally try and extract these colors into variables, that will not change the way
+         its being output, but it will make it easier to change colors, if you want to , because you will have to change the color only in the
+         variable.*/
+
+        //string title = "Protein Intake Week: 1";
+        //string pipe = "|";
+        //string horLine = "======================";
+        //float firstFloat = 80.88557f;
+        //float secondFloat = 94.56465f;
+        //float thirdFloat = 78.67893f;
+        //float fourthFloat = 88.66654f;
+        //float fifthFloat = 88.6466f;
+        //float sixthFloat = 76.777f;
+        //float seventhFloat = 91.85759f;
+        //float totalFloat = 55.77853f;
+
+        //ForegroundColor is the text
+        //BackgroundColor is the background
+        //I created methods ColorfulMessages and ColorfulBackGround
+        //to assign variables you can also do the following:
+        //CoonsoleColor charColor = ConsoleColor.White then 
+        //call it by: Console.ForegroundColor = charColor;
+
+
+        //ColorfulMessages(ConsoleColor.Blue);
+        //ColorfulBackGround(ConsoleColor.Cyan);
+        //Console.WriteLine(pipe + title + pipe);
+        //Console.WriteLine(pipe + horLine + pipe);
+        //ColorfulMessages(ConsoleColor.Green);
+        //ColorfulBackGround(ConsoleColor.DarkGray);
+        //Console.Write(pipe + "{0, 22:C2}", firstFloat);
+        //Console.WriteLine(pipe);
+        //Console.Write(pipe + "{0, 22:C2}", secondFloat);
+        //Console.WriteLine(pipe);
+        //Console.Write(pipe + "{0, 22:C2}", thirdFloat);
+        //Console.WriteLine(pipe);
+        //Console.Write(pipe + "{0, 22:C2}", fourthFloat);
+        //Console.WriteLine(pipe);
+        //Console.Write(pipe + "{0, 22:C2}", fifthFloat);
+        //Console.WriteLine(pipe);
+        //Console.Write(pipe + "{0, 22:C2}", sixthFloat);  
+        //Console.WriteLine(pipe);
+        //Console.Write(pipe + "{0, 22:C2}", seventhFloat);
+        //Console.WriteLine(pipe);
+        //ColorfulMessages(ConsoleColor.Blue);
+        //Console.WriteLine(pipe + horLine + pipe);
+        //ColorfulMessages(ConsoleColor.Green);
+        //ColorfulBackGround(ConsoleColor.White);
+        //Console.Write(pipe + "Total" + "{0, 17:C2}", totalFloat);
+        //Console.WriteLine(pipe);
+
+        //Console.ResetColor();
+        //Thread.Sleep(5000);  //use System.Threading
+        //Console.Clear(); //clears everything from the console
+
+
+
+
+
+        /* 6.Make the size of the console variable so that it always wraps around the text that is output on it and no additional free spaces.
+         */
+
+        string title = "Protein Intake Week: 1";
+        string pipe = "|";
+        string horLine = "======================";
+        float firstFloat = 80.88557f;
+        float secondFloat = 94.56465f;
+        float thirdFloat = 78.67893f;
+        float fourthFloat = 88.66654f;
+        float fifthFloat = 88.6466f;
+        float sixthFloat = 76.777f;
+        float seventhFloat = 91.85759f;
+        float totalFloat = 55.77853f;
+
+        //ForegroundColor is the text
+        //BackgroundColor is the background
+        //I created methods ColorfulMessages and ColorfulBackGround
+        //to assign variables you can also do the following:
+        //CoonsoleColor charColor = ConsoleColor.White then
+        //call it by: Console.ForegroundColor = charColor;
+
+        //how to position, change size of console
+
+       Console.SetWindowSize(70, 20); //width or height change at the same time
+       //Console.SetBufferSize(50, 20); //width and height cannot be less than window size
+       //Console.SetWindowPosition(10, 5); // left and top, //will get error if there are no scroller
+
+        ColorfulMessages(ConsoleColor.Blue);
+        ColorfulBackGround(ConsoleColor.Cyan);
+        Console.WriteLine(pipe + title + pipe);
+        Console.WriteLine(pipe + horLine + pipe);
+        ColorfulMessages(ConsoleColor.Green);
+        ColorfulBackGround(ConsoleColor.DarkGray);
+        Console.Write(pipe + "{0, 22:C2}", firstFloat);
+        Console.WriteLine(pipe);
+        Console.Write(pipe + "{0, 22:C2}", secondFloat);
+        Console.WriteLine(pipe);
+        Console.Write(pipe + "{0, 22:C2}", thirdFloat);
+        Console.WriteLine(pipe);
+        Console.Write(pipe + "{0, 22:C2}", fourthFloat);
+        Console.WriteLine(pipe);
+        Console.Write(pipe + "{0, 22:C2}", fifthFloat);
+        Console.WriteLine(pipe);
+        Console.Write(pipe + "{0, 22:C2}", sixthFloat);
+        Console.WriteLine(pipe);
+        Console.Write(pipe + "{0, 22:C2}", seventhFloat);
+        Console.WriteLine(pipe);
+        ColorfulMessages(ConsoleColor.Blue);
+        Console.WriteLine(pipe + horLine + pipe);
+        ColorfulMessages(ConsoleColor.Green);
+        ColorfulBackGround(ConsoleColor.White);
+        Console.Write(pipe + "Total" + "{0, 17:C2}", totalFloat);
+        Console.WriteLine(pipe);
+
+        Console.ResetColor();
+        Thread.Sleep(5000);  //use System.Threading
+        Console.Clear(); //clears everything from the console
+    }
+
+    static void ColorfulMessages(ConsoleColor color)
+    {
+        Console.ForegroundColor = color;
+       // Console.ResetColor();
+    }
+
+    static void ColorfulBackGround(ConsoleColor color)
+    {
+        Console.BackgroundColor = color;
+        // Console.ResetColor();
+    }
+
+}
---
+++ C#Beginners/CSharpPrimitiveDataTypeArrays/Program.cs
@@ -0,0 +1,267 @@
+﻿using System;
+using System.Text;
+
+//Session 3
+
+class EntryPoint
+{
+    static void Main()
+    {
+        //Array --somethings that can hold value
+
+        //string iAmArray = "array";
+
+        //Console.WriteLine(iAmArray);
+        //Console.WriteLine(iAmArray[3]); //will print a, the third index 0,1,2,3,4
+        //                                //Console.WriteLine(iAmArray[5]); //this will throw an error because 5 is outside of the array
+
+        ////create an array of string
+
+        //string[] iAmStringArray = new string[3];    //initiate the array
+
+        //iAmStringArray[0] = "First item";
+        //iAmStringArray[2] = "Third item";
+
+        //change the value
+
+        //iAmStringArray[0] = "First";  //can't do this. Cant change a string that already exists
+
+
+
+        //string iAmArray = "array";
+
+        //string[] iAmStringArray = { "aaa", "bbb", "ccc", "ddd", "eee" }; //use curly brackets
+
+        //Console.WriteLine(iAmStringArray[3]);
+
+        ////get length of array , how many items inside the array
+
+        //Console.WriteLine(iAmStringArray.Length);
+        //Console.WriteLine(iAmArray.Length);
+
+
+        ////create array for other data types
+        ////int[] integerArray = new int[5];
+        //int[] integerArray = { 1, 2, 3, 4, 43257 };
+
+
+        //integerArray[3] = 123;
+
+
+        //***** Convert any Data Type to a String
+
+        //int someCrazyNumber = 4536334;
+        //Console.WriteLine(someCrazyNumber);
+        //Console.WriteLine(someCrazyNumber.GetType()); //get data type
+
+        //string theNumberWasStringed = someCrazyNumber.ToString();
+        //Console.WriteLine(theNumberWasStringed);
+        //Console.WriteLine(theNumberWasStringed.GetType()); //get data type
+        //Console.WriteLine(theNumberWasStringed[3]); //should print out 6 from string '4536334'
+
+
+        //bool theTrue = true;
+
+        //Console.WriteLine(theTrue.ToString()[2]); //print the u in 'true'
+
+
+        //********************String tricks
+        //learn if string contains character, Index of, Last index of, lower/uppercase, to char array, new string
+
+        //string coffeMug = "CoFfee";
+
+
+
+        //Console.WriteLine(coffeMug.Contains("o")); //will display true if the string coffe contains o
+        //Console.WriteLine(coffeMug.Contains("l")); //false
+        //Console.WriteLine(coffeMug.Contains("of")); //true
+
+        //Console.WriteLine(coffeMug.IndexOf("f"));
+        //Console.WriteLine(coffeMug.IndexOf("of")); //only gives the index of the first character
+
+
+        //Console.WriteLine(coffeMug.LastIndexOf('f')); // displays 3 
+
+        //Console.WriteLine(coffeMug.ToLower());
+        //Console.WriteLine(coffeMug.ToUpper());
+
+        //char[] coffeCharArray = coffeMug.ToCharArray();
+
+        //Console.WriteLine(coffeMug[2]);
+        //Console.WriteLine(coffeCharArray[2]);
+
+
+        //Console.WriteLine(new string('-', 40)); //will print 40 hyphens or whatever you choose, can place this anywhere in your code
+
+        //string dashes = new string('-', 40);
+        //Console.WriteLine(dashes);
+        //Console.WriteLine(dashes);
+        //Console.WriteLine(dashes);
+        //Console.WriteLine(dashes);
+
+
+        // **********Type  Casting, Converting , Parsing 
+
+        //int number = 5;
+        //string word = "number";
+        //char x = 'x';
+        //float floating = 3.14F;
+
+        ////type casting 
+        ////int someInt = floating; //will not work
+        //int someInt = (int)floating; //will work
+
+        ////convert
+        //string convertedChar = x.ToString();
+
+
+        ////parse
+        //// string wordd = "7";
+        //string wordd = null;
+        ////number = Convert.ToInt32(wordd); //prints  7
+        //number = int.Parse(wordd);  //prints 7
+        //                            //The difference between Convert and Parse is that Convert will not give you an error when the string is null
+
+        //// number = (int)char.GetNumericValue(x);
+
+        //Console.WriteLine(number);
+
+        //Generate random number
+
+        //Random rng = new Random();
+
+        //Console.WriteLine(rng.Next());
+        //Console.WriteLine(rng.Next(1,15));
+        //Console.WriteLine(rng.Next(1,15));
+        //Console.WriteLine(rng.Next(1,15));
+
+
+        //   Homework 2:
+
+        //1. Declare a variable for each of the data types in this section and assign an appropriate value. - Int, Float, Double, Bool, Char, String, Object
+
+        //int intVariable = 1972;
+        //Console.WriteLine(intVariable);
+
+        //float floatVariable = 3.14f;
+        //Console.WriteLine(floatVariable);
+
+        //double doubleVariable = 3.1111111;  //dont need to add a d
+        //Console.WriteLine(doubleVariable);
+
+        //bool booleanVariable = true;
+        //Console.WriteLine(booleanVariable);
+
+        //char charVariable = 'c';
+        //Console.WriteLine(charVariable);
+
+        //string stringVariable = "mystring";
+        //Console.WriteLine(stringVariable);
+
+        //object objectVariable = "12345";
+        //Console.WriteLine(objectVariable);
+
+
+
+
+        //2. Read two double numbers from the console.Write a program to successfully compare these numbers with precision of 0.0001.e.g. 3.006 and 3.1 false, 3.00007 and 3.00008 true
+        //3.006 and 3.1 false, 3.00007 and 3.00008 true
+
+
+        //double precisionValue = 0.0001;
+
+        //Read two double numbers from the console
+        //Console.Write("firstDouble:  ");
+        //double firstDouble = double.Parse(Console.ReadLine());
+        //Console.Write("secondDouble:  ");
+        //double secondDouble = double.Parse(Console.ReadLine());
+
+        ////Write a program to successfully compare these numbers with precision of 0.0001.e.g. 3.006 and 3.1 false, 3.00007 and 3.00008 true
+        //Console.WriteLine(Math.Round(firstDouble, 4) >=  Math.Round(secondDouble, 4));
+
+
+
+
+
+        //3. Create a character variable and assign it with the character with Unicode 91.
+
+        //char charVar = '\u0091';  //will produce a ?
+        // Console.WriteLine(charVar);
+
+
+
+        //4.Declare a boolean variable isMyCatAlive and give it an appropriate value, depending on if your cat is or isn't alive.
+
+        //bool isMyCatAlive = false;
+        // if (isMyCatAlive)
+        //{
+        //    Console.WriteLine("The cat is alive");
+        //}
+        //else
+        //{
+        //    Console.WriteLine("Cat is not alive");
+        //}
+
+
+
+        //5. Declare two string variables and assign them with “C#” and “Course”. Declare an object variable and assign it with the concatenation of the first two variables
+        //   (mind adding an interval between the words). Declare a third string variable and initialize it with the value of the object variable (you should perform type casting).
+
+        // Declare two string variables and assign them with “C#” and “Course”.
+        //string firstString = "C#";
+        //string secondString = " Course";
+
+        ////Declare an object variable and assign it with the concatenation of the first two variables
+        //object combineStrings = firstString + " " + secondString;
+
+        ////string thirdString = combineStrings; //does not work without casting
+        //string thirdString = (string)combineStrings; //casting
+
+        //Console.WriteLine(thirdString);
+
+
+        //6. Draw on the console a 5 by 5 rectangle with the symbol "?".
+
+        //string dashes = new string('-', 40);
+        //Console.WriteLine(dashes);
+
+        //char recSymbol = '*';
+        //string rectTopAndBottom = new string(recSymbol, 5);
+        //string recSide = recSymbol + "   " + recSymbol;
+
+        //Console.WriteLine(rectTopAndBottom);
+        //for (int i = 0; i < 3; i++)
+        //{
+        //    Console.WriteLine(recSide);
+        //}
+        //Console.WriteLine(rectTopAndBottom);
+
+
+        //7. Read two integer values from the console, save their values in variables, and then exchange the values of these variables.
+        //   If the first variable had the value 1 and the second variable had the value 2, after the exchange, 
+        //the first variable should get the value 2 and the second variable should get the value 1.
+
+        Console.Write("First Integer: ");
+        int firstInt = int.Parse(Console.ReadLine());
+        Console.Write("Second Integer: ");
+        int secondInt = int.Parse(Console.ReadLine());
+        int tempInt;
+
+        Console.WriteLine("Before exchange, the firstInt = {0} secondInt = {1}", firstInt, secondInt);
+
+        bool isEqual = (firstInt == secondInt);        
+
+        if (isEqual)
+        {
+            Console.WriteLine("The numbers are the same");
+        }
+        else
+        {
+            tempInt = firstInt;  
+            firstInt = secondInt;
+            secondInt = tempInt;
+            Console.WriteLine("Ater exchange, the firstInt = {0} secondInt = {1}", firstInt, secondInt);
+        }
+    }
+}
+
